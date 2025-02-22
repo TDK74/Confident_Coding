d68eb6e4d26914219b1f8c1513de1c5917b7ef7a
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-02-22 22:48:14 +0200

    Added a new example file from the book.

---
+++ Python_examples_3.py
@@ -0,0 +1,32 @@
+myList = [1, 2, 3, 4]
+myList.remove(4)
+print(myList)
+myList.insert(3, 4)
+print(myList)
+myList.pop(3)
+print(myList)
+myList.insert(3, 4)
+print(myList)
+print('\n')
+
+myList.remove(1)
+print(myList)
+myList.insert(0, 1)
+print(myList)
+myList.pop(0)
+print(myList)
+myList.insert(0, 1)
+print(myList)
+print('\n')
+
+myList.insert(4, 5)
+print(myList)
+print('\n')
+
+myList[0] = 2
+print(myList)
+myList.remove(5)
+print(myList)
+myList.insert(2, 3)
+print(myList)
+print('\n')
