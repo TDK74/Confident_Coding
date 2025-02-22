38ebbe7246eb996a9edbedbc79aa6736e8a101f9
Tsvetan (Цецо) <98406593+TDK74@users.noreply.github.com>
2025-02-22 22:48:59 +0200

    Added a new example file from the book.

---
+++ Python_examples_5.py
@@ -0,0 +1,18 @@
+i = 0
+while i < 10:
+  print(i)
+  i = i + 1
+print('\n')
+
+j = 10
+while j >= 0:
+  print(j)
+  j = j - 1
+print('\n')
+
+names = ["John", "Paul", "Ringo", "George"]
+i = 0
+while i < len(names):
+  print("Hello " + names[i])
+  i = i + 1
+print('\n')
